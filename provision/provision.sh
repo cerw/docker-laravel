d2f18c9cfeb571f775a227e5959aebbe8d5043b3
Petr Cervenka <petr@nanosolutions.io>
2017-07-01 16:44:29 +0800

    run vnc

--- provision/provision.sh
+++ provision/provision.sh
@@ -50,6 +50,17 @@ curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key ad
     echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list && \
     apt-get -yqq update && \
     apt-get -yqq install google-chrome-stable x11vnc ca-certificates
+
+# vnc
+
+VNC_STORE_PWD_FILE=~/.vnc/passwd
+if [ ! -e "${VNC_STORE_PWD_FILE}" -o -n "${VNC_PASSWORD}" ]; then
+    mkdir -vp ~/.vnc
+
+    # the default VNC password is 'hola'
+    x11vnc -storepasswd ${VNC_PASSWORD:-hola} ${VNC_STORE_PWD_FILE}
+fi
+
 # ------------------------------------------------------------------------------
 # Clean up
 # ------------------------------------------------------------------------------
