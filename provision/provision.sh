e8550de23080068a51db622f37ac9b0e98e481fb
Petr Cervenka <petr@nanosolutions.io>
2017-07-16 21:06:28 +0800

    adding memcached for tagable cache

--- provision/provision.sh
+++ provision/provision.sh
@@ -10,7 +10,10 @@ apt-get update && apt-get upgrade -y -o Dpkg::Options::="--force-confold"
 # ------------------------------------------------------------------------------
 
 # install PHP
-apt-get -y install curl php-zip zip unzip bzip2 php-cli php-imagick imagemagick git php-curl php-xml  php7.0-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-xdebug
+apt-get -y install curl php-zip zip unzip bzip2 php-cli php-imagick imagemagick git \
+php-curl php-xml  php7.0-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-xdebug \
+memcached php-memcached
+
 #phpdismod xdebug
 #hpdismod -s cli xdebug
 
