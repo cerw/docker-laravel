aa0d012c9b1f03659795061dbdbdb96cb211d6db
Petr Cervenka <petr@nanosolutions.io>
2017-09-07 18:02:29 +0800

    try 7.1

--- provision/provision.sh
+++ provision/provision.sh
@@ -11,12 +11,12 @@ apt-get update && apt-get upgrade -y -o Dpkg::Options::="--force-confold"
 
 # install PHP
 apt-get -y install curl php-zip zip unzip bzip2 php-cli php-imagick imagemagick git \
-php-curl php-xml  php7.0-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-xdebug \
+php-curl php-xml  php7.1-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-xdebug \
 memcached php-memcached
 
 #phpdismod xdebug
 #hpdismod -s cli xdebug
-
+#erv
 # ------------------------------------------------------------------------------
 # Composer PHP dependency manager
 # ------------------------------------------------------------------------------
