7591564802cf5bd56ab141c80c6e296611f409e8
Petr Cervenka <petr@nanosolutions.io>
2017-11-30 20:14:22 +0800

    Adding phpgd

--- provision/provision.sh
+++ provision/provision.sh
@@ -10,7 +10,7 @@ apt-get update && apt-get upgrade -y -o Dpkg::Options::="--force-confold"
 # ------------------------------------------------------------------------------
 
 # install PHP
-apt-get -y install curl php-zip zip unzip bzip2 php-cli php-imagick imagemagick git \
+apt-get -y install curl php-zip zip php7.0-gd unzip bzip2 php-cli php-imagick imagemagick git \
 php-curl php-xml  php7.0-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-xdebug \
 memcached php-memcached php7.0-soap
 
